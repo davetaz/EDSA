ade618939930394e5bbaf0199ae55f2c01ec4048
Dave Tarrant <davetaz@theodi.org>
2016-03-18 17:33:24 +0000

    Adding thematic analyser code

---
+++ calais/README.md
@@ -0,0 +1,19 @@
+Thematic analyser
+=================
+
+You need an API key to open calais in a config file (api_key.php)
+
+<?php 
+	
+	$APIKEY = "";
+
+?>
+
+
+Content must be in a single file, one line per item (e.g. content.txt)
+
+run the analyser and give the argument as the file name where the content is.
+
+php run_analysis.php content.txt > output.html
+
+Done
---
+++ calais/run_analysis.php
@@ -0,0 +1,51 @@
+<?php
+	include('api_key.php');
+	require('opencalais.php');
+	$file = $argv[1];
+
+	$handle = fopen($argv[1],"r");
+	while ($line = fgets($handle)) {
+		$oc = new OpenCalais($api_key);
+		$entities[] = $oc->getEntities($line);
+		sleep(1);
+	}
+	fclose($handle);
+	
+	for ($i=0;$i<count($entities);$i++) {
+		$current = $entities[$i];
+		foreach($current as $type => $values) {
+			for ($j=0;$j<count($values);$j++) {
+				$value = $values[$j];
+				$output[$type][$value][] = $i;
+			}
+		}
+	}
+	echo "<style>
+h3 {
+        width: 100%;
+        padding: 10px;
+        background-color: lightblue;
+}
+</style>";	
+	echo "<h2>Total records: " . count($entities) . "</h2>";
+	foreach($output as $type => $topics) {
+		echo "<h3>" . $type . "</h3>";
+		foreach($topics as $topic => $values) {
+			if (count($values) > 2) {
+				echo '<h4><a onClick="showHide(' . $topic . ');">' . $topic . ' [' . count($values) . ']</a></h4>';
+				echo '<ul id="'.$topic.'_lines" style="display: block">';
+				for ($i=0;$i<count($values);$i++) {	
+					echo '<li>' . getLine($values[$i]) . '</li>';
+				}
+				echo '</ul>';
+			}
+		}
+	}
+
+function getLine($line) {
+	global $file;
+	$lines = file($file);
+	return $lines[$line];
+}
+
+?>
